9e4b55a28e65b044d19ff973b5ee0e4b48f120b2
Tomeoka <62282992+ttt496@users.noreply.github.com>
2021-10-05 16:22:36 +0900

    Update Network.py

--- Network.py
+++ Network.py
@@ -84,3 +84,46 @@ class Transformer(torch.nn.Module):
         x = self.out_fc(d.flatten(start_dim=1))
         
         return x
+    
+    class Transformer_multiIO_infer(torch.nn.Module):
+    def __init__(self, dim_val, dim_attn, n_X_features, n_Y_features, dec_seq_len, out_seq_len, n_decoder_layers = 1, n_encoder_layers = 1, n_heads = 1):
+        super(Transformer_multiIO_infer, self).__init__()
+        self.dec_seq_len = dec_seq_len
+        self.out_seq_len = out_seq_len
+        self.n_Y_features = n_Y_features
+        #Initiate encoder and Decoder layers
+        self.encs = nn.ModuleList()
+        for i in range(n_encoder_layers):
+            self.encs.append(EncoderLayer(dim_val, dim_attn, n_heads))
+        
+        self.decs = nn.ModuleList()
+        for i in range(n_decoder_layers):
+            self.decs.append(DecoderLayer(dim_val, dim_attn, n_heads))
+        
+        self.pos = PositionalEncoding(dim_val)
+        
+        #Dense layers for managing network inputs and outputs
+        self.enc_input_fc = nn.Linear(n_X_features, dim_val)
+        self.dec_input_fc = nn.Linear(n_X_features, dim_val)
+
+        ###########output fc ##############
+        #self.out_fc = nn.Linear(dec_seq_len * dim_val, out_seq_len)
+        self.out_fc = nn.Linear(dec_seq_len * dim_val, out_seq_len * n_Y_features)
+
+    
+    def forward(self, x): 
+        #encoder
+        e = self.encs[0](self.pos(self.enc_input_fc(x)))
+        for enc in self.encs[1:]:
+            e = enc(e)
+        
+        #decoder
+        d = self.decs[0](self.dec_input_fc(x[:,-self.dec_seq_len:]), e)
+        for dec in self.decs[1:]:
+            d = dec(d, e)
+            
+        #output
+        x = self.out_fc(d.flatten(start_dim=1))
+        x = torch.reshape( x , ( x.size()[0], self.out_seq_len,  self.n_Y_features,) )
+        
+        return x
